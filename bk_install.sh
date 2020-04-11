4b27b266f6a8ea6f9e003c22d65b510ce6bc6108
Liuyu <yetlau@163.com>
2020-04-12 00:02:17 +0800

    update  def

--- bk_install.sh
+++ bk_install.sh
@@ -10,6 +10,7 @@ esac
 
 
 data="/data/install/"
+num="10.0.0"
 
 one_install () {
     while true:   
@@ -30,29 +31,52 @@ sed -ri "s/[a-z]{6}.[A-Z]{4}.[A-Z]{5}.[A-Z]{4}..[a-z]{1,}[0-9]{1,}+[A-Z]{1,}.*/e
                }  
 
 
-ping () {
- a=`ping  -c 2  -w 2 $ip2  && ping  -c 2  -w 2 $ip3`
- b=`ping  -c 2  -w 2 $ip2  && ping  -c 2  -w 2 $ip4`
- c=`ping  -c 2  -w 2 $ip3  && ping  -c 2  -w 2 $ip4`
+ping2 () {
+    while true:   
+    do
+      read -p "请输入中控机ip"      ip2
+      ping  -c 2  -w 2 $ip1
+        if [ $? ! -eq 0 ];then
+         echo  "ip格式错误，无法通信此ip" 
+         break
+        fi
+    done
+        }
+
+ping3 () {
+    while true:   
+    do
+      read -p "请输入第一个节点ip"  ip3
+      ping  -c 2  -w 2 $ip1
+        if [ $? ! -eq 0 ];then
+         echo  "ip格式错误，无法通信此ip" 
+         break
+        fi
+    done
+        }
+
+ping4 () {
+    while true:   
+    do
+      read -p "请输入第二个节点ip"  ip4
+      ping  -c 2  -w 2 $ip1
+        if [ $? ! -eq 0 ];then
+         echo  "ip格式错误，无法通信此ip" 
+         break
+        fi
+    done
         }
 
 
-
-
-
 more_install () {
-    while true:
-    do
-      read -p "请输入中控机ip"      ip2
-      read -p "请输入第一个节点ip"  ip3
-      read -p "请输入第二个节点ip"  ip4
-      ping  -c 2  -w 2 $ip2
-    done
+ping1
+ping2
+ping3
 cp $datainstall.config.new.sample  $datainstall.config
 sed -i "8,15s/^/#/"  $datainstall.config
-sed -i "4s/10.0.0.1/$ip2/1"  $datainstall.config
-sed -i "5s/10.0.0.2/$ip3/1"  $datainstall.config
-sed -i "6s/10.0.0.3/$ip4/1"  $datainstall.config
+sed -i "4s/$num.1/$ip2/1"  $datainstall.config
+sed -i "5s/$num.2/$ip3/1"  $datainstall.config
+sed -i "6s/$num.3/$ip4/1"  $datainstall.config
 sed -i "s/bk.com/cloud.bk.com/"  $dataglobals.env 
 sed -ri "s/[a-z]{6}.[A-Z]{4}.[A-Z]{5}.[A-Z]{4}..[a-z]{1,}[0-9]{1,}+[A-Z]{1,}.*/export PAAS_ADMIN_PASS='123456'/"   $dataglobals.env
                  }
